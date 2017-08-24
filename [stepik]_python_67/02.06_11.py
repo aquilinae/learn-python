eaccdb5237534504c1f4286f67481fe79d728c96
ornichola <ornichola@users.noreply.github.com>
2017-08-25 00:12:04 +0300

    Finishing exercises from Week 2

--- [stepik]_python_67/02.06_11.py
+++ [stepik]_python_67/02.06_11.py
@@ -16,3 +16,36 @@ Sample Output:
 14 23 22 21 8
 13 12 11 10 9
 '''
+
+n = int(input())
+
+x = 0
+y = 0
+dx = 1
+dy = 0
+
+# Инициализация квадратной матрицы nxn с элементами типа None
+matrix = [[None] * n for _ in range(n)]
+
+for i in range(1, n**2+1):
+    matrix[x][y] = i
+    nx = x + dx
+    ny = y + dy
+    # Проверяем, не вышла ли позиция за границы и не занята ли уже ячейка
+    if (0 <= nx < n) and (0 <= ny < n) and (not matrix[nx][ny]):
+        x = nx
+        y = ny
+    else:
+        #dx, dy = -dy, dx
+        # Если позиция за границей и ячейка не пуста, разворачиваем матрицу на 90 градусов
+        swap = -dy
+        dy = dx
+        dx = swap
+        x = x + dx
+        y = y + dy
+
+# Выводим заполненную матрицу        
+for y in range(n):
+    for x in range(n):
+        print(matrix[x][y], end = ' ')
+    print()
