fbdcf56282f18c2ff85c9c8c3522fe30efb4f503
Nikolai <ornichola@users.noreply.github.com>
2017-08-30 18:28:10 +0300

    Exercise 5(05) from Week 3

--- [stepik]_python_67/03.07_05.py
+++ [stepik]_python_67/03.07_05.py
@@ -34,24 +34,36 @@ Sample Output:
 '''
 
 class_rawinfo = {}
-new_heights = 0
-new_students = 0
+#new_heights = 0
+#new_students = 0
 class_info = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
+#class_info = []
 
-#with open('03.07_05_input.tsv') as in_f_obj:
-	#string = in_f_obj.readline().rstrip().split('	')
-n = int(input())
-for _ in range(n):
-	string = input().split('	')
-	if string[0] not in class_rawinfo:
-		class_rawinfo[string[0]] = [int(string[2]), 1]
-	elif string[0] in class_rawinfo:
-		new_heights = class_rawinfo[string[0]][0] + int(string[2])
-		new_students = class_rawinfo[string[0]][1] + 1
-		class_rawinfo[string[0]] = [new_heights, new_students]
-
-#with open('03.07_05_input.tsv', 'w') as out_f_obj:
+with open('03.07_05_input.txt') as in_f_obj:
+	for line in in_f_obj:
+		#print(line)
+		string = line.rstrip().split('\t')
+		#print(string)
+#n = int(input())
+#for _ in range(n):
+	#string = input().split('	')
+		if string[0] not in class_rawinfo:
+			class_rawinfo[string[0]] = [int(string[2]), 1]
+		elif string[0] in class_rawinfo:
+			heights = class_rawinfo[string[0]][0] + int(string[2])
+			students = class_rawinfo[string[0]][1] + 1
+			class_rawinfo[string[0]] = [heights, students]
 
 for k, v in class_rawinfo.items():
 	#print(k, str(v[0] / v[1]))
-	class_info.insert((int(k)-1), (v[0] / v[1]))
+	#list.pop([int(k)-1])
+	class_info[int(k)-1] = v[0] / v[1]
+	#print(class_rawinfo)
+	#print(class_info)
+
+with open('03.07_05_output.txt', 'w') as out_f_obj:	
+	for i in range(len(class_info)):
+		#print(i+1, str(class_info[i]))
+		output = str(i+1) + ' ' + str(class_info[i]) + '\n'
+		#print(output)
+		out_f_obj.write(output)
