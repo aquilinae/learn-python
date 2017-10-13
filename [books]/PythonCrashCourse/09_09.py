b27233742acd7a5d956f1d4088e30c9d80d40380
Nikolai <ornichola@users.noreply.github.com>
2017-10-13 09:28:44 +0300

    Ch9 Ex9 Implementing main classes and methods

---
+++ [books]/PythonCrashCourse/09_09.py
@@ -0,0 +1,56 @@
+class Car():
+    """Простая модель автомобиля."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class Battery():
+    """Простая модель аккумулятора электромобиля."""
+
+    def __init__(self, battery_size=70):
+        """Инициализирует атрибуты аккумулятора."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+        def get_range(self):
+            """Выводит приблизительный запас хода для аккумулятора."""
+            if self.battery_size == 70:
+                range = 240
+            elif self.battery_size == 85:
+                range = 270
+            message = "This car can go approximately " + str(range)
+            message += " miles on a full charge."
+            print(message)
+
+class ElectricCar(Car):
+    """Представляет аспекты машины, специфические для электромобилей."""
+
+    def __init__(self, make, model, year):
+        """
+        Инициализирует атрибуты класса-родителя.
+        Затем инициализирует атрибуты, специфические для электромобиля.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
