8293b9d2d3dbc3aff569ddcda5c19da78e50f686
Nikolai <ornichola@users.noreply.github.com>
2017-10-13 11:28:49 +0300

    Chapter 9 Exercise 10

--- [books]/PythonCrashCourse/ch09_ex10.py
+++ [books]/PythonCrashCourse/ch09_ex10.py
@@ -1,4 +1,13 @@
-from ch09_ex06_ex07_ex08 import Restaurant
+from ch09_ex10_restaraunt import Restaurant
+from ch09_ex10_user import Admin
+
+# Task 10
+
+print("\n# TASK 10\n")
 
 samovar = Restaurant("Samovar", "Russian")
 samovar.describe_restaurant()
+
+# Task 11
+
+print("\n# TASK 11\n")
