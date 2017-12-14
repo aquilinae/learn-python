fe341e367de15a5d260b39a0ba7c36000fb409e3
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-14 23:46:21 +0300

    head-first-python initial

---
+++ head-first-python/odd.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+odds = [ 1, 3, 5, 7, 9, 13, 15, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+    print("This minute seems a little odd.")
+else:
+    print("Not and odd minute.")
